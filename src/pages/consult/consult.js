a8416f2493c309b7dd359a81f32a85e67267e7ee
fanxiaolong <developer@lunarhook.com>
2024-07-17 19:18:11 +0800

    u

--- src/pages/consult/consult.js
+++ src/pages/consult/consult.js
@@ -1,35 +1,44 @@
-import { View, Text, ScrollView, Image, Input } from '@tarojs/components'
+import { View, Text, ScrollView, Image, CoverImage, CoverView, Button } from '@tarojs/components'
 import React, { Component } from 'react'
-import { AtList, AtInput, AtGrid, AtIcon, AtTabBar } from "taro-ui"
+import { AtToast } from "taro-ui"
 import './consult.scss'
 
 import bg from '../../../img/consult.bg.jpg'
+import button from '../../../img/consult.button.jpg'
 
-var cur = new Array()
-cur.push(logo)
+let consultpagethis = null
 export default class consult extends Component {
-  handleClick(e) {
+  constructor(props) {
+    super(props)
+    this.state={
+      open:false,
+      text:"123"
+    }
+    consultpagethis = this
+  }
+  OnTap(e) {
     const x = e.changedTouches[0].clientX
     const y = e.changedTouches[0].clientY
-   
-    var cur = Math.floor(x/65) + 1
-    console.log(`点击位置：X=${x}, Y=${y} ,C=${cur}`)
+
+    var cur = Math.floor(x / 65) + 1
+    console.log(`点击位置：X=${x}, Y=${y} ,C=${cur}`,consultpagethis)
+    consultpagethis.setState({open:true,text:consultpagethis.state.text+"!"})
+
   }
   render() {
     return (
-      <View className={"imagecontain"} animation={this.state.animationData}>
-      <ScrollView >
-      <Image className="imageslogan" mode='widthFix' src={bg}></Image>
-            <View className={"contain"}  >
-            <CustomWrapper>
-                <Text className="text"     space="nbsp" >
-                  {"    "+sloganshow[this.state.cur].contect}
-                </Text>
-                </CustomWrapper>
-              </View>
-
-      </ScrollView>
-    </View>
+      <View >
+        <ScrollView >
+          <CoverView >
+            <CoverView className='dailog'>
+              <Button>{consultpagethis.state.text}</Button>
+              </CoverView>
+            <CoverImage src={bg} />
+            <CoverImage src={button} className='controls' onTap={this.OnTap.bind(this)}/>
+          </CoverView>
+          <AtToast isOpened={true} text="沟通" ></AtToast>
+        </ScrollView>
+      </View>
     )
   }
 }
