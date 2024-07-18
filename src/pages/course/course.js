ed1cf27e1cfb5eb2f6c619490c4156bf3348979a
fanxiaolong <developer@lunarhook.com>
2024-07-18 21:09:29 +0800

    Update course.js

--- src/pages/course/course.js
+++ src/pages/course/course.js
@@ -1,5 +1,6 @@
 import { View, Text, ScrollView, Image, Input } from '@tarojs/components'
 import React, { Component } from 'react'
+import Taro from '@tarojs/taro'
 import { AtList, AtInput, AtGrid, AtIcon, AtTabBar } from "taro-ui"
 import './course.scss'
 import banner from '../../../img/course.bar.jpg'
@@ -12,10 +13,14 @@ export default class course extends Component {
 
     var cur = Math.floor(x / 65) + 1
     console.log(`点击位置：X=${x}, Y=${y} ,C=${cur}`)
+    Taro.navigateToMiniProgram({
+      appId: 'wx8dd67b2e08d5f85a', // 替换为目标小程序的appId
+      //path: 'pages/index/index' // 目标小程序内的页面路径
+    })
   }
   render() {
     return (
-      <View className='index'>
+      <View className='index' onClick={this.handleClick.bind(this)}>
         <View className='components-page'>
           <Image className='components-page'
             src={banner}
