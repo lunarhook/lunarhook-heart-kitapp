09bbbc6f68e4aa80a9b2ff0024f94dfc7daa707b
丛雨楠 <congyunan@126.com>
2024-08-02 04:48:49 +0800

    feat:更新脚手架

---
+++ README.md
@@ -0,0 +1,23 @@
+# lunarhook-heart-kitapp
+
+比心家长端小程序
+
+## 项目开发
+
+### 安装依赖
+
+```bash
+yarn install
+```
+
+### 项目启动
+
+```bash
+yarn dev:weapp
+```
+
+## 项目构建
+
+```bash
+yarn build:weapp
+```
--- config/index.js
+++ config/index.js
@@ -4,45 +4,57 @@ import devConfig from './dev'
 import prodConfig from './prod'
 
 // https://taro-docs.jd.com/docs/next/config#defineconfig-辅助函数
-export default defineConfig(async (merge, { command, mode }) => {
+export default defineConfig(async (merge) => {
   const baseConfig = {
-    projectName: 'lunarhook-fingerheart-kitapp',
-    date: '2024-7-15',
+    // 项目名称
+    projectName: 'lunarhook-heart-kitapp',
+    // 项目创建日期
+    date: '2024-8-2',
+    // 设计稿尺寸
     designWidth: 750,
+    // 设计稿尺寸换算规则
     deviceRatio: {
       640: 2.34 / 2,
       750: 1,
       375: 2,
       828: 1.81 / 2
     },
+    // 项目源码目录
     sourceRoot: 'src',
+    // 项目产出目录
     outputRoot: 'dist',
+    // Taro 插件配置
     plugins: [],
-    defineConstants: {
-    },
+    // 全局变量设置
+    defineConstants: {},
+    // 文件 copy 配置
     copy: {
-      patterns: [
-      ],
-      options: {
-      }
+      patterns: [],
+      options: {}
     },
+    // 框架，react，nerv，vue, vue3 等
     framework: 'react',
+    // 编译工具
     compiler: 'webpack5',
+    // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
     cache: {
-      enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+      enable: true
     },
+    // 小程序端专用配置
     mini: {
       postcss: {
+        autoprefixer: {
+          enable: true,
+          config: {},
+        },
         pxtransform: {
           enable: true,
-          config: {
-
-          }
+          config: {}
         },
         url: {
           enable: true,
           config: {
-            limit: 1024 // 设定转换尺寸上限
+            maxSize: 1024 // 设定转换尺寸上限
           }
         },
         cssModules: {
@@ -51,40 +63,6 @@ export default defineConfig(async (merge, { command, mode }) => {
             namingPattern: 'module', // 转换模式，取值为 global/module
             generateScopedName: '[name]__[local]___[hash:base64:5]'
           }
-        }
-      }
-    },
-    h5: {
-      publicPath: '/',
-      staticDirectory: 'static',
-      output: {
-        filename: 'js/[name].[hash:8].js',
-        chunkFilename: 'js/[name].[chunkhash:8].js'
-      },
-      miniCssExtractPluginOption: {
-        ignoreOrder: true,
-        filename: 'css/[name].[hash].css',
-        chunkFilename: 'css/[name].[chunkhash].css'
-      },
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {}
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
-        }
-      }
-    },
-    rn: {
-      appName: 'taroDemo',
-      postcss: {
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         }
       }
     }
